734708ca275304f3e1eb96be25cbdda5e1db197a
Swapvp <swapneshpawar1994@gmail.com>
2024-06-05 11:00:13 +0530

    ip finder

---
+++ src/Components/Map.jsx
@@ -0,0 +1,49 @@
+// Map.js
+
+import React, { useEffect, useState } from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
+import { RiUserLocationFill } from "react-icons/ri";
+
+const API_KEY =
+  "pk.eyJ1IjoiZGVtb3N3YXAiLCJhIjoiY2x3cTBlb2pmMXdreDJrcnlyam1iNWZuMiJ9.m-veGnQc9G-O0qlso6dXQw";
+
+const Map = ({ lat, lon }) => {
+  // Setting up the initial viewport of the map
+  const [viewport, setViewport] = useState({
+    latitude: lat,
+    longitude: lon,
+    zoom: 14,
+    bearing: 0,
+    pitch: 0,
+    width: "100%",
+    height: "100%",
+  });
+
+  // Viewport re-renders whenever latitude
+  // and longitude changes
+  useEffect(() => {
+    const a = { ...viewport };
+    a.latitude = lat;
+    a.longitude = lon;
+    setViewport(a);
+  }, [lat, lon]);
+
+  return (
+    <div className="map">
+      <ReactMapGL
+        mapboxApiAccessToken={API_KEY}
+        {...viewport}
+        onViewportChange={(viewport) => setViewport(viewport)}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+      >
+        <Marker latitude={lat} longitude={lon}>
+          <div className="mark">
+            <RiUserLocationFill size="25px" color="blue" />
+          </div>
+        </Marker>
+      </ReactMapGL>
+    </div>
+  );
+};
+
+export default Map;
